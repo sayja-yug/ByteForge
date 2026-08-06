f01f1f342123398e55381ef53a1a94f0048f6fe3
sayja-yug <yugsayja312@gmail.com>
2026-08-06 22:39:47 +0530

    fixed

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+if [ -d "ByteForge" ]; then
+  cd ByteForge
+fi
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
